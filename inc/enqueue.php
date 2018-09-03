d4262e9c4b16af75cf9fc5d28e4416b16cdbce0a
kshitijbhatt <kbhatt14king@gmail.com>
2018-09-03 22:19:22 +0530

    Corrected jquery migrate error and upgraded to bootstrap 4.1.3

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,10 +4,9 @@
  *
  * @package understrap
  */
-
 if ( ! function_exists( 'understrap_scripts' ) ) {
 	/**
-	 * Load theme's JavaScript sources.
+	 * Load theme's JavaScript and CSS sources.
 	 */
 	function understrap_scripts() {
 		// Get the theme data.
@@ -16,18 +15,15 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		
 		$css_version = $theme_version . '.' . filemtime(get_template_directory() . '/css/theme.min.css');
 		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), $css_version );
-		wp_enqueue_script( 'scrollMagic', get_template_directory_uri() . '/js/ScrollMagic.min.js');
-		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), $theme_version, true);
+		wp_enqueue_script( 'jquery');
 		
 		$js_version = $theme_version . '.' . filemtime(get_template_directory() . '/js/theme.min.js');
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
-		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
 	}
 } // endif function_exists( 'understrap_scripts' ).
-
 add_action( 'wp_enqueue_scripts', 'understrap_scripts' );
 
 // Add admin scripts for custom javascripts
