5fe8585752cde1398f8ba5c3cd3534e1022b3154
kshitijbhatt <kbhatt14king@gmail.com>
2018-09-04 11:08:36 +0530

    homepage corrected for agp header

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -7,7 +7,7 @@
  * @package understrap
  */
 
-get_header('sticky');
+get_header('agp');
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
@@ -17,26 +17,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<div class="row">
 
-      			<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'col-xl-2 d-none d-xl-flex bg-white rounded navbar sidebar sidebarNav small  ',
-						'container_id'    => 'navbarNavSidbar',
-						'menu_class'      => 'sidebar-sticky',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'walker'          => new understrap_WP_Bootstrap_Navwalker(),
-
-					)
-				); ?>
-
-
-
-				<main class="col-xl-10 col-lg-12 ml-sm-auto px-xl-3 d-flex " id="main" role="main">
-          
-     
-        
-					<div>
 						<?php while ( have_posts() ) : the_post(); ?>
 
 						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
@@ -51,11 +31,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						?>
 
 					<?php endwhile; // end of the loop. ?>
-					</div>
-				</main><!-- #main -->
-
-			</div><!-- #primary -->
-
+							
 		</div><!-- .row end -->
 
 	</div><!-- Container end -->
