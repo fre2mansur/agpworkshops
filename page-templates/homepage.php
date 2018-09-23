c4bdd27a704acb14e77e07e284373a636991050b
kshitijbhatt <kbhatt14king@gmail.com>
2018-09-24 00:17:06 +0530

    Testing cards for homepage

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -14,7 +14,7 @@ $get_plugin_gallery_table = $wpdb->prefix . "advance_green_plugin_gallery";
 $gallery = $wpdb->get_results( "SELECT * FROM $get_plugin_gallery_table ORDER BY RAND() LIMIT 12", OBJECT_K);
 ?>
 
-<div class="<?php echo esc_attr( $container ); ?>" id="homepageContent">
+<div class="<?php echo esc_attr( $container ); ?>">
 
 <!--Gallery Starts-->
 		<?php if($gallery){ ?>
