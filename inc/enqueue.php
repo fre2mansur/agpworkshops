13cd029aad53a80b05a7f03b00c1af3f6cc8bc27
kshitijbhatt <kbhatt14king@gmail.com>
2018-09-25 14:03:42 +0530

    changing delivery method for images with src set on homepage

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -16,6 +16,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		$css_version = $theme_version . '.' . filemtime(get_template_directory() . '/css/theme.min.css');
 		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), $css_version );
 		wp_enqueue_script( 'jquery');
+		
 
 		//Hoem page Portfolio assets
 		wp_enqueue_script( 'jquery-mixitup', get_template_directory_uri() . '/js/jquery.mixitup.min.js', array(), '1.0.0', true );
