7c3429583046d3d503bd1f454aa7c1a9493b4912
kshitijbhatt <kbhatt14king@gmail.com>
2018-09-25 16:24:25 +0530

    correction in header margins

--- header-agp.php
+++ header-agp.php
@@ -85,65 +85,65 @@ if ( wp_is_mobile() ) {
   ?>
 
   <!-- Desktop Menu Starts -->
-  <nav class="navbar navbar-expand-lg sticky-top shadow-sm navbar-light bg-white py-0">
-  <?php $custom_logo_id = get_theme_mod( 'custom_logo' );
-            $logo_url = wp_get_attachment_image_src( $custom_logo_id , 'full',['class'=>"d-lg-none"] );
-            $style = ' background-image: url("'.$logo_url[0].'"); background-size: contain; background-repeat:no-repeat; background-position:center center; min-height:50px;
-              min-width:235px; width:50%; vertical-align:middle; transition: all 0.2s linear;';
-      ?>
+  <nav class="navbar navbar-expand-lg sticky-top shadow-sm navbar-light bg-white py-0 margin-bottom-60">
+            <?php $custom_logo_id = get_theme_mod( 'custom_logo' );
+                      $logo_url = wp_get_attachment_image_src( $custom_logo_id , 'full',['class'=>"d-lg-none"] );
+                      $style = ' background-image: url("'.$logo_url[0].'"); background-size: contain; background-repeat:no-repeat; background-position:center center; min-height:50px;
+                        min-width:235px; width:50%; vertical-align:middle; transition: all 0.2s linear;';
+            ?>
             <?php if ( ! has_custom_logo() ) { ?>
-        <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mx-auto d-lg-none " title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-      <?php } else { ?>
-        <a rel='home' href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mx-auto d-lg-none" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"> <div id="logo" style='<?php echo $style; ?>'></div></a>
-      <?php } ?>
+              <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mx-auto d-lg-none " title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+            <?php } else { ?>
+              <a rel='home' href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mx-auto d-lg-none" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"> <div id="logo" style='<?php echo $style; ?>'></div></a>
+            <?php } ?>
    
-   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#desktopNavbar" aria-controls="desktopNavbar" aria-expanded="true" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-    <div class="navbar-collapse collapse" id="desktopNavbar">
-    <ul class="navbar-nav container-fluid">
-      <!-- The Left Menu goes here -->
-      <?php 
-        wp_nav_menu(
-          array(
-            'theme_location'  => 'left',
-            'container_class' => 'ml-lg-auto',
-            'container_id'    => 'nav-left',
-            'menu_class'      => 'navbar-nav', //Ul class
-            'menu_id'         => 'main-menu',
-            'fallback_cb'     => '',
-            'walker'          => new understrap_WP_Bootstrap_Navwalker(),
-          ) 
-        );
-      ?>
-    
-      <!-- Your site title as branding in the menu -->
-      <?php $custom_logo_id = get_theme_mod( 'custom_logo' );
-            $logo_url = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-            $style = ' background-image: url("'.$logo_url[0].'"); background-size: contain; background-repeat:no-repeat; background-position:center center; min-height:50px;
-              min-width:235px; width:50%; vertical-align:middle; transition: all 0.2s linear;';
-      ?>
-      <?php if ( ! has_custom_logo() ) { ?>
-        <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mx-auto d-none d-lg-inline-block " title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-      <?php } else { ?>
-        <a rel='home' href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mx-auto d-none d-lg-inline-block" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"> <div id="logo" style='<?php echo $style; ?>'></div></a>
-      <?php } ?>
-      
-      <!-- The Right Menu goes here -->
-      <?php 
-        wp_nav_menu(
-          array(
-            'theme_location'  => 'right',
-            'container_class' => 'mr-lg-auto',
-            'container_id'    => 'nav-right',
-            'menu_class'      => 'navbar-nav', //Ul class
-            'menu_id'         => 'right-menu',
-            'fallback_cb'     => '',
-            'walker'          => new understrap_WP_Bootstrap_Navwalker(),
-          ) 
-        );
-      ?>
-    </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#desktopNavbar" aria-controls="desktopNavbar" aria-expanded="true" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse" id="desktopNavbar">
+            <ul class="navbar-nav container-fluid">
+              <!-- The Left Menu goes here -->
+              <?php 
+                wp_nav_menu(
+                  array(
+                    'theme_location'  => 'left',
+                    'container_class' => 'ml-lg-auto',
+                    'container_id'    => 'nav-left',
+                    'menu_class'      => 'navbar-nav', //Ul class
+                    'menu_id'         => 'main-menu',
+                    'fallback_cb'     => '',
+                    'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+                  ) 
+                );
+              ?>
+            
+              <!-- Your site title as branding in the menu -->
+              <?php $custom_logo_id = get_theme_mod( 'custom_logo' );
+                    $logo_url = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+                    $style = ' background-image: url("'.$logo_url[0].'"); background-size: contain; background-repeat:no-repeat; background-position:center center; min-height:50px;
+                      min-width:235px; width:50%; vertical-align:middle; transition: all 0.2s linear;';
+              ?>
+              <?php if ( ! has_custom_logo() ) { ?>
+                <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mx-auto d-none d-lg-inline-block " title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+              <?php } else { ?>
+                <a rel='home' href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand mx-auto d-none d-lg-inline-block" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"> <div id="logo" style='<?php echo $style; ?>'></div></a>
+              <?php } ?>
+              
+              <!-- The Right Menu goes here -->
+              <?php 
+                wp_nav_menu(
+                  array(
+                    'theme_location'  => 'right',
+                    'container_class' => 'mr-lg-auto',
+                    'container_id'    => 'nav-right',
+                    'menu_class'      => 'navbar-nav', //Ul class
+                    'menu_id'         => 'right-menu',
+                    'fallback_cb'     => '',
+                    'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+                  ) 
+                );
+              ?>
+            </div>
   </nav>
   <!-- Desktop Menu Ends -->
 <?php
