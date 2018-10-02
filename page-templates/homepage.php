73d6f202f9b549fa51982e173e50b3ba30fad1f4
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-02 15:51:07 +0530

    filtering based on start date - Test 8

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -143,7 +143,57 @@ $container = get_theme_mod( 'understrap_container_type' );
 						</div>
 					</div> 
 				<?php 	}
-								} endwhile;?>
+								
+							}
+							else{
+								?>
+								<div class="card <?php echo $tax; ?>" data-cat="<?php echo $tax;?>">
+						<a class="d-block" href="#workshop_<?php echo $randPostIDsForAccordion;?>" data-toggle="collapse" aria-expanded="false" aria-controls="workshop_<?php echo $post->ID?>">
+						<?php the_post_thumbnail('medium', ['class' =>"card-img-top"]); ?>
+						</a>
+						<div class="card-body pb-0">
+							<a class="decoration-none" data-toggle="collapse" href="#workshop_<?php echo $randPostIDsForAccordion; ?>" role="button" aria-expanded="false" aria-controls="workshop_<?php echo the_ID(); ?>" >
+								<div class="d-flex justify-content-between header">
+									<h5 class="card-title"><?php the_title()?></h5>
+									<a class="collapsed" data-toggle="collapse" href="#workshop_<?php echo $randPostIDsForAccordion; ?>" role="button" name="header" aria-expanded="false" aria-controls="workshop_<?php echo the_ID(); ?>" >
+										<span class="arrow"></span>
+									</a>
+								</div> 
+							</a>
+							<h6 class="card-subtitle text-muted mb-2 pb-2"><?php the_terms( $post->ID, 'workshop_category' ); ?></h6>
+							<div class="collapse my-2" id="workshop_<?php echo $randPostIDsForAccordion; ?>"  data-parent="#accordion">
+								<p class="card-text"><?php echo wp_strip_all_tags(get_field('brief_intro'));?></p>
+								<div class="d-flex justify-content-start mb-3">
+									<a href="<?php the_permalink(); ?>">Know more</a>
+									<a href="#" class="ml-5">Register now</a>
+								</div>     
+							</div>
+						</div>
+						<hr class="p-0 m-0 mt-2">
+						<div class="footer d-flex justify-content-between m-0 px-4">
+						<?php ?>
+							<div class="py-3">
+								<span class="mr-auto">Starts - </span>
+								<strong><?php
+									$date = get_field('date_selector'); echo $date['start_date'];
+								
+								?></strong>
+							</div>
+							<span class="line border border-gray mx-auto"></span>
+							<div class="py-3 pl-2">
+								<span class="mr-auto">Ends -</span>
+								<strong><?php
+								$get_the_schedule_type = get_field('select_the_schedule_type');
+								$date = get_field('date_selector');
+								echo $date['end_date'];
+								
+								
+								?></strong>
+							</div>
+						</div>
+					</div> 
+							<?php }
+								 endwhile;?>
 				</div>
 			</div> 
 		 </div> 
