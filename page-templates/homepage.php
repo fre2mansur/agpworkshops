62f56a91e9b91e287ca98160cee271bda0ededba
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-04 01:24:07 +0530

    filtering based on start date - Test 91

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -148,11 +148,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 								<span class="mr-auto">Ends -</span>
 								<strong><?php
 								
-								$get_the_schedule_type = get_field('select_the_schedule_type');
-								$number_of_weeks = get_field('number_of_weeks');
-								if($get_the_schedule_type == "daily"){				
-																		
-								}
+								$endDate = get_sub_field('end_date');
+								echo $endDate;
 								
 								
 								?></strong>
