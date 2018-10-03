0a1c3974d7b1a863d97079194010da086e7e78bc
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-03 23:21:55 +0530

    filtering based on start date - Test 65

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -137,8 +137,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 									
 									
 									$startDate = get_post_meta( $post->ID,'start_date_wp',true);
-									print_r (date('d-m-Y', strtotime($startDate)));
-									echo $startDate;
+									print_r (date('d/m/Y', strtotime($startDate)));
+									
 									
 								?></strong>
 							</div>
