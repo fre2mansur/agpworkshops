edae678735d0a8e20b0acb5f8526912eafd66ed3
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-03 11:50:06 +0530

    filtering based on start date - Test 28

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -92,7 +92,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						
 						$dates = get_field('start_date_repeater'); 
 						if(have_rows('start_date_repeater')){
-							while ( have_rows('start_date_repeater') ) :
+							while ( have_rows('start_date_repeater') ) : the_row();
 
 								foreach($dates as $date){
 					
