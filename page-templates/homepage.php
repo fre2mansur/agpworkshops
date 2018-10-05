0326280fe38ef1384ea6869eea43964d96aeaf49
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-05 22:13:58 +0530

    repetitive dates test - 47

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -89,13 +89,9 @@ $container = get_theme_mod( 'understrap_container_type' );
           'post_status' => 'publish' );
 
        
-				$workshops = $wpdb->get_results( "SELECT * FROM $wpdb->postmeta WHERE meta_key LIKE 'start_date_wp' ORDER BY meta_value ASC LIMIT 9" );
-				  
-
-				$meta_query = new WP_Meta_Query( $meta_query_args );
-				$postStartDate = null;
+				$workshops = $wpdb->get_results("SELECT * FROM $wpdb->postmeta WHERE meta_key LIKE 'start_date_wp' ORDER BY meta_value ASC LIMIT 9" );
+		  		$postStartDate = null;
 				foreach($workshops as $post){
-					var_dump($post);
 						$postStartDate = $post->meta_value;
 						$post = $post->post_id;
 						$dates = get_field('start_date_repeater'); 
