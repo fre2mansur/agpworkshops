0a58646a3162a1fb326a1f74089aa4288662ba6b
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-05 17:17:28 +0530

    repetitive dates test -26

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -91,8 +91,8 @@ $container = get_theme_mod( 'understrap_container_type' );
        
 				$workshops = $wpdb->get_results("SELECT * FROM $wpdb->postmeta WHERE meta_key LIKE 'start_date_wp' ORDER BY 'meta_value' ASC" );
 
-				foreach($workshops as $post){
-						echo $post->post_id ."<br>";
+				foreach($workshops as $postFancy){
+						echo $postFancy->post_id ."<br>";
 				}
 				while ( $workshop_query->have_posts() ) :
 
