e8484b1fad6b671d3cbf820c74ee082b53544810
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-05 22:32:37 +0530

    repetitive dates test - 51

--- inc/extras.php
+++ inc/extras.php
@@ -432,7 +432,7 @@ function convert_start_to_standard_wp_meta($post_id) {
   // also note, that this is not an ACF field and will not
   // appear when editing posts, it is just a db field that we
   // will use for searching
-  $meta_key = 'start_date_wp';
+  $meta_key = 'workshop_date_wp';
    
   // the next step is to delete any values already stored
   // so that we can update it with new values
@@ -470,7 +470,8 @@ function convert_start_to_standard_wp_meta($post_id) {
       add_post_meta($post_id, $meta_key, $startDate.",".$endDate, false);
        
       // add it to the values we've already saved
-      $saved_values[$startDate] = $startDate;
+	  $saved_values[$startDate] = $startDate;
+	  $saved_values[$endDate] = $endDate;
        
     } // end while have rows
   } // end if have rows
@@ -483,62 +484,3 @@ function convert_start_to_standard_wp_meta($post_id) {
     start_date_wp meta key in a simple more simple WP_Query()
    
 */
-
-function convert_end_to_standard_wp_meta($post_id) {
-   
-	// pick a new meta_key to hold the values of the start_date field
-	// I generally name this field by suffixing _wp to the field name
-	// as this makes it easy for me to remember this field name
-	// also note, that this is not an ACF field and will not
-	// appear when editing posts, it is just a db field that we
-	// will use for searching
-	$meta_key = 'end_date_wp';
-	 
-	// the next step is to delete any values already stored
-	// so that we can update it with new values
-	// and we don't need to worry about removing a value
-	// when it's deleted from the ACF repeater
-	delete_post_meta($post_id, $meta_key);
-	 
-	// create an array to hold values that are already added
-	// this way we won't add the same meta value more than once
-	// because having the same value to search and filter by
-	// would be pointless
-	$saved_values = array();
-	 
-	// now we'll look at the repeater and save any values
-	if (have_rows('start_date_repeater', $post_id)) {
-	  while (have_rows('start_date_repeater', $post_id)) {
-		the_row();
-		 
-		// get the value of this row
-		$endDate = get_sub_field('end_date',false,false);
-		 
-		// see if this value has already been saved
-		// note that I am using isset rather than in_array
-		// the reason for this is that isset is faster than in_array
-		if (isset($saved_values[$endDate])) {
-		  // no need to save this one we already have it
-		  continue;
-		}
-		 
-		// not already save, so add it using add_post_meta()
-		// note that we are using false for the 4th parameter
-		// this means that this meta key is not unique
-		// and can have more then one value
-		add_post_meta($post_id, $meta_key, $endDate, false);
-		 
-		// add it to the values we've already saved
-		$saved_values[$endDate] = $endDate;
-		 
-	  } // end while have rows
-	} // end if have rows
-  } // end function
-   
-  /*
-	  15 lines of code and now instead of dealing with complicated filters
-	  and "LIKE" queries and modifying the WHERE portion of the query
-	  and slowing down our site, instead we can simply use the
-	  start_date_wp meta key in a simple more simple WP_Query()
-	 
-  */
--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -88,15 +88,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 		  
           'post_status' => 'publish' );
 
-	   
-				  $str = "20181108,20181109";
-				  $arrayDATE = explode(',', $str);
-
-				  var_dump($arrayDATE);
-
-				$workshops = $wpdb->get_results("SELECT * FROM $wpdb->postmeta WHERE meta_key LIKE 'start_date_wp' or meta_key LIKE 'end_date_wp' ORDER BY meta_value ASC LIMIT 9" );
+       
+				$workshops = $wpdb->get_results("SELECT * FROM $wpdb->postmeta WHERE meta_key LIKE 'start_date_wp' ORDER BY meta_value ASC LIMIT 9" );
 		  		$postStartDate = null;
 				foreach($workshops as $post){
+						$postStartDate = $post->meta_value;
 						$post = $post->post_id;
 						$dates = get_field('start_date_repeater'); 
 						
