aa8af65cccb7d4f1f69e7c33e300eeb85999b1cc
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-06 10:21:11 +0530

    repetitive dates test - 68

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -89,14 +89,14 @@ $container = get_theme_mod( 'understrap_container_type' );
         //   'post_status' => 'publish' );
 
 		  $today = date('Ymd');
-		  $metakey = start_date_wp;
+		  $metakey = 'start_date_wp';
 				$workshops = $wpdb->get_results(
 					$wpdb->prepare("SELECT * FROM $wpdb->postmeta 
 					INNER JOIN $wpdb->posts ON ($wpdb->posts.ID = $wpdb->postmeta.post_id)  
 					WHERE meta_key LIKE %s 
 					AND (meta_value > '$today' or meta_value = '$today' AND $wpdb->posts.post_status = 'publish') 
 					ORDER BY meta_value ASC LIMIT 9", $metakey ));
-					
+
 		  		$postStartDate = null;
 				foreach($workshops as $post){
 						$postStartDate = $post->meta_value;
