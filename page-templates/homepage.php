fd772a94748da0492d0d471c2d8885a14c74b762
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-08 14:38:24 +0530

    push footer to bottom

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -12,7 +12,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 ?>
 
-<div class="<?php echo esc_attr( $container ); ?> ">
+<div class="<?php echo esc_attr( $container ); ?> min-height">
 
 <!--Gallery Starts-->
 		<?php
