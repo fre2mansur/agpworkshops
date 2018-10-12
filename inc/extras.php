a33fabd66a6d8769b3d6ecf2831f4ab87f4760dc
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-12 11:39:37 +0530

    testing new slug for workshops

--- inc/extras.php
+++ inc/extras.php
@@ -113,7 +113,7 @@ if ( ! function_exists ( 'understrap_post_nav' ) ) {
  * Load AGP Workshops post type
 
 */
-require get_template_directory() . '/inc/admin_menu_templates/agp_workshops.php';
+require get_template_directory() . '/inc/admin_menu_templates/workshops.php';
 
 /**
  * Load AGP Facilitators post type
