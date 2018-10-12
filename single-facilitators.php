d51537c2fd9d2cf7f74362fb0bfd3969083590a2
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-12 15:55:28 +0530

    single facilitator page added

--- single-facilitators.php
+++ single-facilitators.php
@@ -6,13 +6,22 @@
  *
  * @package understrap  
  */
-get_header(); ?>
+get_header(); 
+$container = get_theme_mod( 'understrap_container_type' );?>
 
 
 <?php while ( have_posts() ) : the_post(); ?>
-
-<h1><?php the_title(); ?></h1>
+<div class="<?php echo esc_attr( $container ); ?>">
+<div class="row">
+<div class="col-3">
+<?php the_post_thumbnail( 'medium', ['class' => 'img-responsive']);  ?>
+</div>
+<div class="col-9">
+<<h2></h2>><?php the_title(); ?></h1>
 <?php the_content(); ?>
 <?php endwhile; ?>
+</div>
+</div>
+</div>
 <?php 
 get_footer(); ?>
