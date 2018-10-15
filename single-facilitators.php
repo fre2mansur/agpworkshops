8994ac343a47588bbe2bc5a7e53746a52862f49b
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-15 15:06:13 +0530

    Adding changes in facilitator page

--- single-facilitators.php
+++ single-facilitators.php
@@ -14,28 +14,31 @@ $container = get_theme_mod( 'understrap_container_type' );?>
 <div class="<?php echo esc_attr( $container ); ?> ">
 <h2 class="brownline-before mb-5">Facilitator</h2>
 <div class="row">
-<div class="col-3">
-<?php the_post_thumbnail( 'medium', ['class' => 'img-responsive']);  ?>
+    <div class="col-3">
+        <?php the_post_thumbnail( 'medium', ['class' => 'img-responsive']);  ?>
+    </div>
+    <div class="col-9">
+        <h2><?php the_title(); ?></h2>
+        <?php $units = get_field('unit_name');
+                                    if($units):
+                                    foreach($units as $unit){?>
+                                        <div class="media mb-3">
+                                            <div class="media-body">
+                                                <small class="text-muted">
+                                                <?php echo $unit->post_title; ?>
+                                                </small>
+                                            </div>
+                                        </div>
+                                    <?php } 
+                                    endif;
+                                ?>
+        <?php the_content(); ?>
+    </div>
 </div>
-<div class="col-9">
-<h2><?php the_title(); ?></h2>
-<?php $units = get_field('unit_name');
-                            if($units):
-                            foreach($units as $unit){?>
-								<div class="media mb-3">
-									<div class="media-body">
-                                        <small class="text-muted">
-                                        <?php echo $unit->post_title; ?>
-                                        </small>
-                                    </div>
-								</div>
-                            <?php } 
-                            endif;
-						?>
-<?php the_content(); ?>
+<div class="row">
+<h2 class="brownline-before mb-5">Related Workshops</h2>
+</div>
+</div>
 <?php endwhile; ?>
-</div>
-</div>
-</div>
 <?php 
 get_footer(); ?>
