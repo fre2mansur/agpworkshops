872df8fa9b815750cd6e3008bd11b040fcd83674
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-17 16:29:19 +0530

    Testing facilitator single page with workshops

--- single-facilitators.php
+++ single-facilitators.php
@@ -12,12 +12,12 @@ $container = get_theme_mod( 'understrap_container_type' );?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 <div class="<?php echo esc_attr( $container ); ?> ">
-<h2 class="brownline-before mb-5">Facilitator</h2>
+<h2 class="brownline-before mb-4">Facilitator</h2>
 <div class="row">
     <figure class="col-md-3 col-12">
-        <?php the_post_thumbnail( 'medium', ['class' => 'img-responsive']);  ?>
+        <?php the_post_thumbnail( 'medium', ['class' => 'img-responsive, w-100']);  ?>
     </figure>
-    <div class="col-lg-9 col-12 my-lg-auto my-2">
+    <div class="col-md-9 col-12 my-md-a0 my-2">
        <article>
            
       
@@ -41,7 +41,7 @@ $container = get_theme_mod( 'understrap_container_type' );?>
     </div>
 </div>
 
-<h2 class="h2 brownline-before my-5">Related Workshops</h2>
+<h2 class="h2 brownline-before my-4">Related Workshops</h2>
 <div class="row">
     <div class="workshop-container" id="accordion">
     <?php 
