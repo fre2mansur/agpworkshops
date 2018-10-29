54609ec26d91784bcf57140ab9910e0cc48882d7
kshitijbhatt <kbhatt14king@gmail.com>
2018-10-29 16:32:46 +0530

    change in what where why

--- single-workshops.php
+++ single-workshops.php
@@ -67,7 +67,7 @@ $payment_details_without_accommodation = get_sub_field('payment_details_without_
 	<div class="who-where-what">
 		<h2 class="brownline-before mb-5"><?php the_title(); ?></h2>
 		
-			<div class="row">
+			<div class="col-11 mx-auto d-flex">
 				<div class="col-lg-4">
 					<h3>What</h3>
 					<p><?php echo $what; ?></p>
