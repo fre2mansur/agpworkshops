b802546d11631cb74c5fe189492226cd09e02bf0
kshitijbhatt <kbhatt14king@gmail.com>
2018-12-17 14:46:44 +0530

    adding headroomJs

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -24,7 +24,8 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		
 		$js_version = $theme_version . '.' . filemtime(get_template_directory() . '/js/theme.min.js');
 		wp_enqueue_script('masonry-js', get_template_directory_uri() . '/js/masonry.min.js', array('jquery'), '4.2.2', true);
-		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array('masonry-js'), $js_version, true );
+		wp_enqueue_script('headroom-js', get_template_directory_uri() . '/js/headroom.min.js', array('jquery'), null, true);
+		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array('masonry-js','headroom-js'), $js_version, true );
 		wp_localize_script('understrap-scripts','gfcustom_Ajax_function',array('ajaxurl' => admin_url('admin-ajax.php')));
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
