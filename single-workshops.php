54aec41ae4cca21cc20d00a00a2b99a3ef713e18
kshitijbhatt <kbhatt14king@gmail.com>
2019-01-18 09:48:51 +0530

    correction in contact information

--- single-workshops.php
+++ single-workshops.php
@@ -229,7 +229,34 @@ $week_four_content = get_field('week_four_content');
 									<div class="card-body unit-details">
 											<h5><?php echo $unit->post_title; ?></h5>
 											<small class="text-muted">
-											<?php  echo wp_trim_words( $unit->post_content, 50, '...'); ?>
+											<table class="table table-borderless">
+												<tbody>
+
+													
+													<tr>
+														<td class="pl-0">
+															<?php echo $unit->post_title ?>
+														</td>
+													</tr>
+													
+													<tr>
+														<td class="pl-0">
+															<?php $unitContactDynamic = get_field("contact_phone_number", $unit->ID);
+															echo '<a href="tel:'.$unitContactDynamic.'">'.$unitContactDynamic.'</a>' ?>
+														</td>
+													</tr>
+												
+												
+													<tr>
+														
+														<td class="pl-0">
+															<?php $unitEmailDynamic = get_field("contact_email", $unit->ID); 
+															echo '<a href="mailto:'.$unitEmailDynamic.'">'.$unitEmailDynamic.'</a>';?>
+														</td>
+													</tr>
+													
+												</tbody>
+											</table>
 											</small>
 									</div>
 								</div>
@@ -386,37 +413,6 @@ $week_four_content = get_field('week_four_content');
 
 					<?php endif; ?>
 		</div>
-		<table class="table table-borderless">
-				<tbody>
-				<?php if($units): ?>
-				<?php foreach($units as $unit){ ?>
-					
-					<tr>
-						<td class="pl-0">
-							<?php echo $unit->post_title ?>
-						</td>
-					</tr>
-					
-					<tr>
-						<td class="pl-0">
-							<?php $unitContactDynamic = get_field("contact_phone_number", $unit->ID);
-							echo '<a href="tel:'.$unitContactDynamic.'">'.$unitContactDynamic.'</a>' ?>
-						</td>
-					</tr>
-				
-				
-					<tr>
-						
-						<td class="pl-0">
-							<?php $unitEmailDynamic = get_field("contact_email", $unit->ID); 
-							echo '<a href="mailto:'.$unitEmailDynamic.'">'.$unitEmailDynamic.'</a>';?>
-						</td>
-					</tr>
-			
-				<?php break; } endif; ?>
-					
-				</tbody>
-			</table>
 		<div class="col-12 p-0 d-flex flex-column text-center">
 			<button class="btn btn-primary mx-auto" type="submit" form="preRegistrationForm" name="registrationFormBtn" id="registrationFormBtn"
 			data-url="<?php echo get_admin_url().'admin-ajax.php'?>">
