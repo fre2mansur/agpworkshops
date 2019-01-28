66ab221a10835941c6d805315ace543ded25f5ab
kshitijbhatt <kbhatt14king@gmail.com>
2019-01-28 17:02:20 +0530

    tsting random image option

--- inc/agp_functions.php
+++ inc/agp_functions.php
@@ -191,7 +191,7 @@ function agpf_card_loop($itemId) {
 			$agp_card_image = wp_get_attachment_image_src( $agp_rand_row_image, 'medium' );
 			
 			?>
-			<figure>
+			<figure class="figure w-100">
 				<img src="<?php echo $agp_card_image[0] ; ?>" alt="<?php echo get_the_title($agp_card_image); ?>" class="card-img-top"/>	
 			</figure>
 	  		 <?php } else {the_post_thumbnail('medium', ['class' =>"card-img-top"]);} ?>
