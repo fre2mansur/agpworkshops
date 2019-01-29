4cb028c124345157e4f2e5a0237cdddcc3f5d49f
kshitijbhatt <kbhatt14king@gmail.com>
2019-01-29 13:39:14 +0530

    correction in shuffle images

--- inc/agp_functions.php
+++ inc/agp_functions.php
@@ -186,8 +186,8 @@ function agpf_card_loop($itemId, $iForRow) {
 		<?php
 			$agp_Image_rows = get_field('shuffle_gallery');
 			
-			if($agp_Image_rows){
-			
+			$agp_row_count = count($agp_Image_rows);
+			if($agp_Image_rows && ($iForRow <= $agp_row_count - 1)){
 			$rand_row = $rows[$iForRow];
 			$agp_rand_row_image = $rand_row['agp_workshop_gallery_images'];
 			$agp_card_image = wp_get_attachment_image_src( $agp_rand_row_image, 'medium' );
