d4923011b9328ddc4446a07d2ae3842f3bc0ca8a
kshitijbhatt <kbhatt14king@gmail.com>
2019-01-29 13:19:11 +0530

    correction in shuffle images

--- inc/agp_functions.php
+++ inc/agp_functions.php
@@ -174,7 +174,7 @@ function agpf_workshop_sql($count="") {
 
 //Loop workshop cards. used in home page, facilitator, unit pages.
 //$itemid = getting key value of foreach, its important.
-function agpf_card_loop($itemId) {
+function agpf_card_loop($itemId, $iForRow=0) {
 	$postId = $itemId->post_id;
 	$workshopStartDate = date('d-m-Y', strtotime($itemId->start_date));
 	$workshopEndDate = date('d-m-Y', strtotime($itemId->end_date));
@@ -186,11 +186,10 @@ function agpf_card_loop($itemId) {
 		<?php
 			$rows = get_field('shuffle_gallery');
 			if($rows){
-			$iForRow = 0; 
 			$rand_row = $rows[$iForRow];
 			$agp_rand_row_image = $rand_row['agp_workshop_gallery_images'];
 			$agp_card_image = wp_get_attachment_image_src( $agp_rand_row_image, 'medium' );
-			$iForRow++;
+			
 			?>
 			<figure class="figure w-100">
 				<img src="<?php echo $agp_card_image[0] ; ?>" alt="<?php echo get_the_title($agp_card_image); ?>" class="card-img-top"/>	
