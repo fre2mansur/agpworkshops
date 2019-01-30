741173ca181786479d7f2a724cec490ecb75ad1e
kshitijbhatt <kbhatt14king@gmail.com>
2019-01-30 17:27:28 +0530

    correction in shuffle images

--- inc/agp_functions.php
+++ inc/agp_functions.php
@@ -191,7 +191,7 @@ function agpf_card_Image_sepration($itemId, $randPostIDsForAccordion, $startDate
 			// 
 			?>
 			<figure class="figure w-100">
-				<img src="<?php echo $agp_card_image[0] ; ?>" alt="<?php echo get_the_title($agp_card_image); ?>" class="card-img-top"/>	
+				<img src="<?php echo $agp_card_image[0]; var_dump($agp_card_image); ?>" alt="<?php echo get_the_title($agp_card_image); ?>" class="card-img-top"/>	
 			</figure>
 	  		 <?php } else { the_post_thumbnail('medium', ['class' =>"card-img-top"]); } ?>
 		</a>
