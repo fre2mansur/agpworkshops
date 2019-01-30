e31f395d2379142a8bb937bc35143a4cebf31277
kshitijbhatt <kbhatt14king@gmail.com>
2019-01-30 17:37:53 +0530

    correction in shuffle images

--- inc/agp_functions.php
+++ inc/agp_functions.php
@@ -183,7 +183,7 @@ function agpf_card_Image_sepration($itemId, $randPostIDsForAccordion, $startDate
 			
 			
 			if($agp_Image_rows){
-			print_r($agp_Image_rows);	
+				
 			$agp_rand_row_image = array_rand($agp_Image_rows, 1);
 			$agp_card_image = wp_get_attachment_image_src( $agp_rand_row_image, 'medium' );
 			
@@ -192,7 +192,7 @@ function agpf_card_Image_sepration($itemId, $randPostIDsForAccordion, $startDate
 			?>
 			<figure class="figure w-100">
 				<?php var_dump($agp_rand_row_image);?>
-				<img src="<?php echo $agp_card_image[0];?>" alt="<?php echo get_the_title($agp_card_image); ?>" class="card-img-top"/>	
+				<img src="<?php echo $agp_rand_row_image['url'];?>" alt="<?php echo get_the_title($agp_card_image); ?>" class="card-img-top"/>	
 			</figure>
 	  		 <?php } else { the_post_thumbnail('medium', ['class' =>"card-img-top"]); } ?>
 		</a>
