f57b4779c3dc46f2b099fe5ecf7e43cf051ff592
kshitijbhatt <kbhatt14king@gmail.com>
2019-02-06 14:35:47 +0530

    testing ajax

--- inc/agp_functions.php
+++ inc/agp_functions.php
@@ -45,7 +45,7 @@ define('ITEMSPERPAGE', '9');
 function agpf_category_filter() {
 	$terms = get_terms('workshop_category',array("order"=>"ASC"));
 	if(!@$_GET[CATPARAM] || @$_GET[CATPARAM] == 'All') { $active = "active";}
-    echo '<li class="nav-item menu-item"><a class="nav-link allClick '.@$active.'" href="">All<a/></li>';
+    echo '<li class="nav-item menu-item"><a class="nav-link allClick '.@$active.'" href="#">All<a/></li>';
 
     foreach ( $terms as $term ) {
         $termname = strtolower($term->term_id);
