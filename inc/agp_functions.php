6131dfcc9b40d9cbf57ae217b9b89c6efb72a73a
kshitijbhatt <kbhatt14king@gmail.com>
2019-02-06 15:13:48 +0530

    testing ajax

--- inc/agp_functions.php
+++ inc/agp_functions.php
@@ -344,11 +344,13 @@ add_action( 'init', 'blog_change_post_object' );
 function ajax_test_function(){
 	check_ajax_referer('custom_nonce_filter','security');
 
-	
+	if(isset($_POST['wCat'])){
+		echo $_POST['wCat'];
+	}
 
 
 	
-	die();
+	die;
 }
  
 add_action('wp_ajax_ajax_test_function', 'ajax_test_function');
