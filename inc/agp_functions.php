9bd04d941b3d6ae3553bfedd03bcd88d7ce76039
kshitijbhatt <kbhatt14king@gmail.com>
2019-02-06 09:55:46 +0530

    correction in scrollable navbar

--- inc/agp_functions.php
+++ inc/agp_functions.php
@@ -58,10 +58,11 @@ function agpf_category_filter() {
 }
 
 function agpf_month_filter() {
-	$months = array('Month'=>'all','Jan'=>'01', 'Feb'=>'02', 'Mar'=>'03', 'Apr'=>'04', 'May'=>'05', 'Jun'=>'06', 'Jul'=>'07','Aug'=>'08','Sep'=>'09','Oct'=>'10','Nov'=>'11','Dec'=>'12');
-	echo '<li class="nav-item menu-item ml-auto"> <a class="nav-link d-none d-md-flex"> Filter by month </a> </li>
+	$months = array('All'=>'All','Jan'=>'01', 'Feb'=>'02', 'Mar'=>'03', 'Apr'=>'04', 'May'=>'05', 'Jun'=>'06', 'Jul'=>'07','Aug'=>'08','Sep'=>'09','Oct'=>'10','Nov'=>'11','Dec'=>'12');
+	echo '<li class="nav-item menu-item ml-auto"> <a class="nav-link d-none d-lg-flex"> Filter by month </a> </li>
 	<li class="nav-item menu-item">
 	<select name="wDate" onchange="parent.window.location=this.value" class="form-control">
+	<option value="" selected disabled hidden>Select Month</option>
 	</li>';
 	foreach ($months as $name => $value) { ?>
 
